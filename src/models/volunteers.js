9ba19570a4a52af49354e5ca7d9f3a0d7f12e1b3
Brendan Cluff <cluffbrendan04@gmail.com>
2026-06-17 08:12:02 -0700

    Add volunteer table and fix volunteer featur

--- src/models/volunteers.js
+++ src/models/volunteers.js
@@ -2,9 +2,9 @@ import db from './db.js';
 
 const addVolunteer = async (accountId, projectId) => {
   const query = `
-    INSERT INTO project_volunteer (account_id, project_id)
+    INSERT INTO project_volunteer (user_id, project_id)
     VALUES ($1, $2)
-    ON CONFLICT (account_id, project_id) DO NOTHING;
+    ON CONFLICT (user_id, project_id) DO NOTHING;
   `;
 
   await db.query(query, [accountId, projectId]);
@@ -13,17 +13,17 @@ const addVolunteer = async (accountId, projectId) => {
 const removeVolunteer = async (accountId, projectId) => {
   const query = `
     DELETE FROM project_volunteer
-    WHERE account_id = $1 AND project_id = $2;
+    WHERE user_id = $1 AND project_id = $2;
   `;
 
   await db.query(query, [accountId, projectId]);
 };
-
+user_id
 const isUserVolunteering = async (accountId, projectId) => {
   const query = `
-    SELECT account_id
+    SELECT user_id
     FROM project_volunteer
-    WHERE account_id = $1 AND project_id = $2;
+    WHERE user_id = $1 AND project_id = $2;
   `;
 
   const result = await db.query(query, [accountId, projectId]);
@@ -43,7 +43,7 @@ const getVolunteerProjectsByUser = async (accountId) => {
     FROM project_volunteer pv
     JOIN projects p ON pv.project_id = p.project_id
     JOIN organization o ON p.organization_id = o.organization_id
-    WHERE pv.account_id = $1
+    WHERE pv.user_id = $1
     ORDER BY p.project_date ASC;
   `;
 
